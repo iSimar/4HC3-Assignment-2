ab97ac9850e9e72945746c9d12674e04254c5571
Simar Singh <hello@simarsingh.com>
2016-11-10 00:20:17 -0500

    find get parameter

--- script.js
+++ script.js
@@ -201,6 +201,17 @@ function getAccount(accNumber, pinNumber){
     }
 }
 
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+
 $( document ).ready(function() {
     if(session){
         $( ".menu-content" ).css("display", "block");
